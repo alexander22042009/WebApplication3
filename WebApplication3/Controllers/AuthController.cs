4657e5908e570d96e24309d5792e592e63c75e8a
alexander22042009 <101943388+alexander22042009@users.noreply.github.com>
2026-03-25 12:06:34 +0200

    Add project files.

---
+++ WebApplication3/Controllers/AuthController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Data.Entities;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager <ApplicationUser> _userManager;
+        private readonly IConfiguration configuration;
+
+        public AuthController(UserManager<ApplicationUser> userManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            this.configuration = configuration;
+        }
+
+
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+
+            }
+
+            var user = await _userManager.FindByNameAsync(model.UserName);
+
+            if (user == null)
+            {
+                return BadRequest(new
+                {
+                    message = "UserName already exist",
+                });
+            }
+
+            var result = await _userManager.CheckPasswordAsync(user, model.Password);
+
+            if (result == false)
+            {
+                return Unauthorized(new
+                {    
+                    message = "Password or UserName doesn't match"
+                });
+            }
+
+            var token = GenerateJWTToken(user);
+
+            return Ok(new
+            {
+                token = token
+            });
+        }
+
+
+    }
+
+
+}
