a273b4e628d9af9f75be028d490b7979ed9dc173
alexander22042009 <101943388+alexander22042009@users.noreply.github.com>
2026-04-16 12:21:09 +0300

    register and login

--- WebApplication3/Data/Entities/ApplicationUser.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace WebApplication3.Data.Entities;
-
-public class ApplicationUser : IdentityUser
-{
-}
--- WebApplication3/Models/Houses/AllHousesViewModel.cs
+++
@@ -1,6 +0,0 @@
-namespace WebApplication3.Models.Houses;
-
-public class AllHousesViewModel
-{
-    public IEnumerable<HouseDetailsViewModel> Houses { get; set; } = new List<HouseDetailsViewModel>();
-}
